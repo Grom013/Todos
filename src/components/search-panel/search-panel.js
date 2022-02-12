0dc2b35e87d6395e86c90140a39f73bc92d98044
Grom013 <89138415+Grom013@users.noreply.github.com>
2022-02-12 17:59:48 +0300

    All project

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import "./search-panel.css";
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+  }
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+  render() {
+    return (
+      <input
+        placeholder="Найти сотрудника"
+        type="text"
+        className="form-control search-input"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
+
+export default SearchPanel;
